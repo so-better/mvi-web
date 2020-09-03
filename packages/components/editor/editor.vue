5604093cd6c2b4758058b07930a36d786a955b80
lovelin0523 <18356201582@163.com>
2020-09-03 23:02:24 +0800

    更新

--- packages/components/editor/editor.vue
+++ packages/components/editor/editor.vue
@@ -111,10 +111,10 @@ export default {
 					'#EC1A0A', '#FF9900', '#FFFF00', '#07C160', '#00FFFF', '#0B73DE', '#9C00FF', '#FF00FF',
 					'#F7C6CE', '#FFE7CE', '#FFEFC6', '#D6EFD6', '#CEDEE7', '#CEE7F7', '#D6D6E7', '#E7D6DE',
 					'#E79C9C', '#FFC69C', '#FFE79C', '#B5D6A5', '#A5C6CE', '#9CC6EF', '#B5A5D6', '#D6A5BD',
-					'#E76363', '#F7AD6B', '#FFD663', '#94BD7B', '#73A5AD', '#6BADDE', '#8C7BC6', '#C67BA5',
-					'#CE0000', '#E79439', '#EFC631', '#6BA54A', '#4A7B8C', '#03A8F3', '#634AA5', '#A54A7B',
+					'#e45649', '#F7AD6B', '#FFD663', '#94BD7B', '#73A5AD', '#6BADDE', '#8C7BC6', '#C67BA5',
+					'#CE0000', '#E79439', '#EFC631', '#50a14f', '#4A7B8C', '#03A8F3', '#634AA5', '#A54A7B',
 					'#9C0000', '#B56308', '#BD9400', '#397B21', '#104A5A', '#085294', '#311873', '#731842',
-					'#630000', '#7B3900', '#846300', '#295218', '#083139', '#003163', '#21104A', '#4A1031'
+					'#630000', '#7B3900', '#986801', '#295218', '#083139', '#003163', '#21104A', '#4A1031'
 				],
 				backColor: [//背景色
 					'#000000', '#505050', '#808080', '#BBBBBB', '#CCCCCC', '#EEEEEE', '#F7F7F7', '#FFFFFF',
@@ -796,6 +796,7 @@ export default {
 			}
 			if(event.keyCode == 9){
 				event.preventDefault();
+				document.execCommand('insertHtml',false,'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;')
 			}
 		},
 		//初始化对象参数方法
