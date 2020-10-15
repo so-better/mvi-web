5621201638970f0cbc777cddb5d64354b9a7612e
lovelin0523 <18356201582@163.com>
2020-10-15 17:00:45 +0800

    更新

--- example/App.vue
+++ example/App.vue
@@ -1,52 +1,6 @@
 <template>
 	<div id="app" ref="app" style="padding: 1rem;">
-		<div>
-			<m-button class="mvi-m-2" v-for="(item,index) in types" :key="'btn-'+item" :type="item">Button</m-button>
-		</div>
-		<div>
-			<m-button class="mvi-m-2" v-for="(item,index) in types" :key="'btn-'+item" plain :type="item">Button</m-button>
-		</div>
-		<div>
-			<m-button class="mvi-m-2" color="#eac3f1" sub-color="#ca37e4">Button</m-button>
-			<m-button class="mvi-m-2" color="#ca37e4" sub-color="#eac3f1">Button</m-button>
-			<m-button class="mvi-m-2" color="#b190ea" sub-color="#864bef">Button</m-button>
-			<m-button class="mvi-m-2" color="#864bef" sub-color="#b190ea">Button</m-button>
-		</div>
-		<div>
-			<m-button class="mvi-m-2" color="#ca37e4" sub-color="#eac3f1" plain>Button</m-button>
-			<m-button class="mvi-m-2" color="#864bef" sub-color="#b190ea" plain>Button</m-button>
-		</div>
-		<div>
-			<m-button class="mvi-m-2" type="success" loading>Button</m-button>
-			<m-button class="mvi-m-2" type="success" loading>
-				Button
-				<template v-slot:load>
-					<m-icon type="load-a" spin/>
-					<span class="mvi-ml-1">LOADING...</span>
-				</template>
-			</m-button>
-			<m-button class="mvi-m-2" type="success" loading>
-				Button
-				<template v-slot:load>
-					<m-icon type="load-b" spin/>
-					<span class="mvi-ml-1">正在全速加载中...</span>
-				</template>
-			</m-button>
-			<m-button class="mvi-m-2" type="success" loading>
-				Button
-				<template v-slot:load>
-					<m-icon type="load-c" spin/>
-					<span class="mvi-ml-1">LOADING...</span>
-				</template>
-			</m-button>
-			<m-button class="mvi-m-2" type="success"  loading>
-				Button
-				<template v-slot:load>
-					<m-icon type="load-d" spin/>
-					<span class="mvi-ml-1">LOADING...</span>
-				</template>
-			</m-button>
-		</div>
+		
 	</div>
 </template>
 
