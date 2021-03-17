4ae7b64772f33a281315816fb591e21af25ba132
lovelin0523 <18356201582@163.com>
2021-03-17 10:59:14 +0800

    更新

--- example/App.vue
+++ example/App.vue
@@ -1,7 +1,21 @@
 <template>
 	<div id="app"><router-view></router-view></div>
 </template>
-
+<script>
+	export default {
+		data(){
+			return {
+				
+			}
+		},
+		mounted() {
+			console.log(this.$util.matchingText('1799-04-09','date'))
+			console.log(this.$util.matchingText('2021/03/19','date'))
+			console.log(this.$util.matchingText('2021.03.19','date'))
+			console.log(this.$util.matchingText('2021年03月03日','date'))
+		}
+	}
+</script>
 <style lang="less">
 #app {
 	height: 100%;
