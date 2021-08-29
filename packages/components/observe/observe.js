c8c2ef225c5788d72624a1c8926b74995c7439df
lovelin0523 <18356201582@163.com>
2021-08-29 11:44:25 +0800

    更新

--- packages/components/observe/observe.js
+++ packages/components/observe/observe.js
@@ -47,12 +47,12 @@ class Observe {
 			let observer = new MutationObserver(mutationList=>{
 				let length = mutationList.length;
 				for(let i = 0;i<length;i++){
-					//监听属性
+					//监听属性变动
 					if(mutationList[i].type == 'attributes' && this.attributes){
-						this.attributesChange(mutationList[i].attributeName,this.$el.getAttribute(mutationList[i].attributeName),mutationList[i].oldValue);
+						this.attributesChange(mutationList[i].attributeName,this.$el.getAttribute(mutationList[i].attributeName),mutationList[i].oldValue)
 					}
 					//监听子节点变动
-					if(mutationList[i].type == 'childList' && this.childList){
+					else if(mutationList[i].type == 'childList' && this.childList){
 						for(let node of mutationList[i].addedNodes){
 							if($util.isElement(node)){
 								this.childNodesChange(node,null)
